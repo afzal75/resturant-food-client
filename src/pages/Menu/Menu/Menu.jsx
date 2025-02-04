727fa3d5edcfee79250f9da1d831fed348874ba5
afzal75 <afzalbhuiyan16@gmail.com>
2025-02-04 11:48:23 +0600

    react helmet implement successfully

---
+++ src/pages/Menu/Menu/Menu.jsx
@@ -0,0 +1,13 @@
+import { Helmet } from "react-helmet-async";
+const Menu = () => {
+  return (
+    <div>
+      <Helmet>
+        <title>Menu</title>
+      </Helmet>
+      <h2>This is menu</h2>
+    </div>
+  );
+};
+
+export default Menu;
