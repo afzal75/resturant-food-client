51d93777590942df324ce82451f5804fa20147e0
afzal75 <afzalbhuiyan16@gmail.com>
2025-02-04 12:35:59 +0600

    menu created successfully

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,11 +1,19 @@
 import { Helmet } from "react-helmet-async";
+import Cover from "../../Shared/Cover/Cover";
+import menuImg from "../../../assets/menu/banner3.jpg";
+import PopularMenu from "../../Home/PopularMenu/PopularMenu";
 const Menu = () => {
   return (
     <div>
       <Helmet>
-        <title>Menu</title>
+        <title>Food | Menu</title>
       </Helmet>
-      <h2>This is menu</h2>
+      <Cover img={menuImg} title="Our Menu"></Cover>
+      <PopularMenu></PopularMenu>
+      <Cover img={menuImg} title="Our Menu"></Cover>
+      <PopularMenu></PopularMenu>
+      <Cover img={menuImg} title="Our Menu"></Cover>
+      <PopularMenu></PopularMenu>
     </div>
   );
 };
