a5e932abba6e440c814726e58774dd55f8d3b84e
afzal75 <afzalbhuiyan16@gmail.com>
2025-02-04 17:11:26 +0600

    order tab created successfully

--- src/pages/Menu/MenuCategory/MenuCategory.jsx
+++ src/pages/Menu/MenuCategory/MenuCategory.jsx
@@ -5,7 +5,7 @@ const MenuCategory = ({ items, title, coverImg }) => {
   return (
     <div className="pt-8">
       {title && <Cover img={coverImg} title={title}></Cover>}
-      <div className="grid md:grid-cols-2 gap-10 mt-16">
+      <div className="grid md:grid-cols-2 gap-10 my-16">
         {items?.map((item) => (
           <MenuItem key={item._id} item={item}></MenuItem>
         ))}
---
+++ src/pages/Order/Order/Order.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import orderCover from "../../../assets/shop/banner2.jpg";
+import Cover from "../../Shared/Cover/Cover";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import useMenu from "../../../hooks/useMenu";
+import OrderTab from "../OrderTab/OrderTab";
+const Order = () => {
+  const [tabIndex, setTabIndex] = useState(0);
+  const [menu] = useMenu();
+
+  const desserts = menu.filter((item) => item.category === "dessert");
+  const soups = menu.filter((item) => item.category === "soup");
+  const salads = menu.filter((item) => item.category === "salad");
+  const pizzas = menu.filter((item) => item.category === "pizza");
+  const drinks = menu.filter((item) => item.category === "offered");
+  return (
+    <div>
+      <Cover img={orderCover} title="Order Food"></Cover>
+      <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+        <TabList className="uppercase">
+          <Tab>Salad</Tab>
+          <Tab>Pizza</Tab>
+          <Tab>Soup</Tab>
+          <Tab>Dessert</Tab>
+          <Tab>Drinks</Tab>
+        </TabList>
+        <TabPanel>
+          <OrderTab items={salads}></OrderTab>
+        </TabPanel>
+        <TabPanel>
+          <OrderTab items={pizzas}></OrderTab>
+        </TabPanel>
+        <TabPanel>
+          <OrderTab items={soups}></OrderTab>
+        </TabPanel>
+        <TabPanel>
+          <OrderTab items={desserts}></OrderTab>
+        </TabPanel>
+        <TabPanel>
+          <OrderTab items={drinks}></OrderTab>
+        </TabPanel>
+      </Tabs>
+    </div>
+  );
+};
+
+export default Order;
